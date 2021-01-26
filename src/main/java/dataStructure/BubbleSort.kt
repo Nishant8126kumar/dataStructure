55aa582b4a2f9f4b15acbf97f8b4778f3e167ed5
Nishant Sharma <nishant.kumar@fretron.com>
2021-01-26 21:30:24 +0530

    bubble sort Example

---
+++ src/main/java/dataStructure/BubbleSort.kt
@@ -0,0 +1,38 @@
+package dataStructure
+
+import java.util.*
+
+class BubbleSort {
+    val scan = Scanner(System.`in`)
+    fun bubbleSort() {
+        val arrayData = arrayListOf<Int>()
+        println("Enter the Size Of Array")
+        val size = scan.nextInt()
+        println("Enter $size Element")
+        for (i in 1..size) {
+            arrayData.add(scan.nextInt())
+        }
+        println("data ${arrayData[0]}")
+        var swap = true
+        while (swap) {
+            swap = false
+            for (j in 0 until size-1) {
+                if (arrayData[j] > arrayData[j+1]) {
+                    val temp = arrayData[j+1]
+                    arrayData[j+1] = arrayData[j]
+                    arrayData[j] = temp
+                    swap=true
+                }
+            }
+        }
+        println("Sorted Data is =:$arrayData")
+
+    }
+}
+
+fun main() {
+    val bubble = BubbleSort()
+    bubble.bubbleSort()
+
+
+}
